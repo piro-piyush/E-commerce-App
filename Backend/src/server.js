718961338a43e4c9b2ecddd7f03dcc7e48e7fef5
piro-piyush <piyush72717272@gmail.com>
2024-11-05 12:34:11 +0530

    updated serves.js after adding categories

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -6,12 +6,10 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const UserRoutes = require('./routes/user_routes');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(bodyParser.json());
-app.use("/api/user", UserRoutes);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 app.use(helmet());
@@ -22,7 +20,13 @@ mongoose.connect(`mongodb+srv://PIYUSH:${process.env.MONGO_PASSWORD}@cluster0.n0
     .then(() => console.log("\nConnected to MongoDB"))
     .catch((err) => console.error("MongoDB connection error:", err));
 
-app.get("/", function (req, res) {
+const UserRoutes = require('./routes/user_routes');
+app.use('/api/user', UserRoutes);
+
+const CategoryRoutes = require('./routes/category_route');
+app.use('/api/category', CategoryRoutes);
+
+app.get('/', function (req, res) {
     res.send("Hi I am home page");
 });
 
