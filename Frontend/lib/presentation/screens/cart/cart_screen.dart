55ca566af1ea4e4c753afd08b569e7ba6323439c
piro-piyush <piyush72717272@gmail.com>
2024-11-09 21:00:11 +0530

    day5 completed

--- Frontend/lib/presentation/screens/cart/cart_screen.dart
+++ Frontend/lib/presentation/screens/cart/cart_screen.dart
@@ -25,70 +25,65 @@ class _CartScreenState extends State<CartScreen> {
         title: const Text("Cart"),
       ),
       body: SafeArea(
-        child: BlocBuilder<CartCubit, CartState>(
-            builder: (context, state) {
+        child: BlocBuilder<CartCubit, CartState>(builder: (context, state) {
+          if (state is CartLoadingState && state.items.isEmpty) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
 
-              if(state is CartLoadingState && state.items.isEmpty) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
+          if (state is CartErrorState && state.items.isEmpty) {
+            return Center(
+              child: Text(state.errorMessage),
+            );
+          }
 
-              if(state is CartErrorState && state.items.isEmpty) {
-                return Center(
-                  child: Text(state.errorMessage),
-                );
-              }
+          if (state is CartLoadedState && state.items.isEmpty) {
+            return const Center(child: Text("Cart items will show up here.."));
+          }
 
-              if(state is CartLoadedState && state.items.isEmpty) {
-                return const Center(
-                    child: Text("Cart items will show up here..")
-                );
-              }
-
-              return Column(
-                children: [
-                  Expanded(
-                      child: CartListView(items: state.items)
-                  ),
-
-                  Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-
-                        Flexible(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-
-                              Text("${state.items.length} items", style: TextStyles.body1.copyWith(fontWeight: FontWeight.bold),),
-                              Text("Total: ${Formatter.formatPrice(Calculations.cartTotal(state.items))}", style: TextStyles.heading3,),
-
-                            ],
+          return Column(
+            children: [
+              Expanded(child: CartListView(items: state.items)),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "${state.items.length} items",
+                            style: TextStyles.body1
+                                .copyWith(fontWeight: FontWeight.bold),
                           ),
-                        ),
-
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width / 2.5,
-                          child: CupertinoButton(
-                            onPressed: () {
-                              // Navigator.pushNamed(context, OrderDetailScreen.routeName);
-                            },
-                            padding: EdgeInsets.all(MediaQuery.of(context).size.width / 22),
-                            color: AppColors.accent,
-                            child: const Text("Place Order"),
+                          Text(
+                            "Total: ${Formatter.formatPrice(Calculations.cartTotal(state.items))}",
+                            style: TextStyles.heading3,
                           ),
-                        ),
-
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              );
-            }
-        ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 2.5,
+                      child: CupertinoButton(
+                        onPressed: () {
+                          // Navigator.pushNamed(context, OrderDetailScreen.routeName);
+                        },
+                        padding: EdgeInsets.all(
+                            MediaQuery.of(context).size.width / 22),
+                        color: AppColors.accent,
+                        child: const Text("Place Order"),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          );
+        }),
       ),
     );
   }
