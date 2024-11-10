c3e827f53e5760ce48e4c937c31c9a836bf51644
piro-piyush <piyush72717272@gmail.com>
2024-11-10 16:20:41 +0530

    products_by_category_screen.dart created

--- Frontend/lib/logic/cubits/products_by_category_cubit/products_by_category_cubit.dart
+++ Frontend/lib/logic/cubits/products_by_category_cubit/products_by_category_cubit.dart
@@ -1,4 +1,6 @@
 import 'package:ecommerce/data/models/category/category_model..dart';
+import 'package:ecommerce/data/models/product/product_model.dart';
+import 'package:ecommerce/data/repositories/product_repository.dart';
 import 'package:ecommerce/logic/cubits/products_by_category_cubit/products_by_category_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -7,7 +9,15 @@ class ProductByCategoryCubit extends Cubit<ProductsByCategoryState>{
   ProductByCategoryCubit(this.category):super(ProductsByCategoryInitialState()) {
     _initialize();
   }
-  void _initialize(){
+  final _productRepository = ProductRepository();
 
+  void _initialize() async{
+    emit(ProductsByCategoryLoadingState(state.products));
+  try{
+      List<ProductModel> products = await _productRepository.fetchProductsByCategory(category.sId!);
+      emit(ProductsByCategoryLoadedState(products));
+  }catch(ex){
+    emit(ProductsByCategoryErrorState(ex.toString(), state.products));
+  }
   }
 }
--- Frontend/lib/presentation/screens/product/products_by_category_screen.dart
+++ Frontend/lib/presentation/screens/product/products_by_category_screen.dart
@@ -1,16 +1,47 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:ecommerce/logic/cubits/products_by_category_cubit/products_by_category_cubit.dart';
+import 'package:ecommerce/logic/cubits/products_by_category_cubit/products_by_category_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProductsByCategoryScreen extends StatefulWidget {
   static const String routeName = "productsByCategoryScreen";
+
   const ProductsByCategoryScreen({super.key});
 
   @override
-  State<ProductsByCategoryScreen> createState() => _ProductsByCategoryScreenState();
+  State<ProductsByCategoryScreen> createState() =>
+      _ProductsByCategoryScreenState();
 }
 
 class _ProductsByCategoryScreenState extends State<ProductsByCategoryScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final cubit = BlocProvider.of<ProductByCategoryCubit>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("${cubit.category.title}"),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+          child: BlocBuilder<ProductByCategoryCubit, ProductsByCategoryState>(
+              builder: (context, state){
+                if (state is ProductsByCategoryLoadingState && state.products.isEmpty){
+                  return Center(child: CircularProgressIndicator(
+                  ),);
+                }
+                if(state is ProductsByCategoryErrorState  && state.products.isEmpty){
+                  return Center(
+                    child: Text(state.errorMessage),
+                  );
+                }
+                if(state is ProductsByCategoryLoadedState && state.products.isEmpty){
+                  return Center(
+                    child: Text("No products found"),
+                  );
+                }
+                return ListView.builder(itemBuilder: itemBuilder);
+              })),
+    );
   }
 }
