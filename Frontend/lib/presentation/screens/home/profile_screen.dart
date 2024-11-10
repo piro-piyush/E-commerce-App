0132038b365facdec4513966bdf7f7e15f5220d1
piro-piyush <piyush72717272@gmail.com>
2024-11-10 17:13:18 +0530

    user profile section created

--- Frontend/lib/presentation/screens/home/profile_screen.dart
+++ Frontend/lib/presentation/screens/home/profile_screen.dart
@@ -1,6 +1,12 @@
 import 'package:ecommerce/core/ui.dart';
+import 'package:ecommerce/data/models/users/user_model.dart';
+import 'package:ecommerce/logic/cubits/user_cubit/user_cubit.dart';
+import 'package:ecommerce/logic/cubits/user_cubit/user_state.dart';
 import 'package:ecommerce/presentation/widgets/gap_widget.dart';
+import 'package:ecommerce/presentation/widgets/link_button.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -13,12 +19,78 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("E-commerce"),
-          actions: [Icon(Icons.shopping_cart,color: AppColors.text,),
-            const GapWidget()
-          ],
-        ));
+      appBar: AppBar(
+        title: const Text("E-commerce"),
+        actions: [
+          Icon(
+            Icons.shopping_cart,
+            color: AppColors.text,
+          ),
+          const GapWidget()
+        ],
+      ),
+      body: BlocBuilder<UserCubit, UserState>(builder: (context, state) {
+        if (state is UserLoadingState) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (state is UserErrorState) {
+          return Center(
+              child: Text(
+            state.errorMessage,
+          ));
+        }
+        if (state is UserLoggedInState) {
+          return userProfile(state.userModel);
+        } else {
+          return Center(
+            child: Text("An error occurred"),
+          );
+        }
+      }),
+    );
   }
 
+  Widget userProfile(UserModel userModel) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                // "${userModel.fullName}",
+                "Piyush Kumar",
+                style: TextStyles.heading3,
+              ),
+              Text(
+                "${userModel.email}",
+                style: TextStyles.body2,
+              ),
+              LinkButton(
+                text: "Edit Profile",
+                color: AppColors.accent,
+                onPressed: () {},
+              )
+            ],
+          ),
+          Divider(),
+          ListTile(
+            onTap: (){},
+            title: Text("My Orders",style: TextStyles.body1,),
+            leading: Icon(CupertinoIcons.cube_box_fill,),
+          ),
+          ListTile(
+            onTap: (){
+              BlocProvider.of<UserCubit>(context).signOut();
+            },
+            title: Text("Sign Out",style: TextStyles.body1.copyWith(color: Colors.red),),
+            leading: Icon(Icons.exit_to_app,color: Colors.red,),
+          )
+        ],
+      ),
+    );
+  }
 }
--- Frontend/lib/presentation/screens/product/products_by_category_screen.dart
+++ Frontend/lib/presentation/screens/product/products_by_category_screen.dart
@@ -1,7 +1,9 @@
 import 'package:ecommerce/logic/cubits/products_by_category_cubit/products_by_category_cubit.dart';
 import 'package:ecommerce/logic/cubits/products_by_category_cubit/products_by_category_state.dart';
+import 'package:ecommerce/presentation/widgets/product_grid_for_details.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class ProductsByCategoryScreen extends StatefulWidget {
   static const String routeName = "productsByCategoryScreen";
@@ -22,25 +24,36 @@ class _ProductsByCategoryScreenState extends State<ProductsByCategoryScreen> {
         title: Text("${cubit.category.title}"),
         centerTitle: true,
       ),
-      body: SafeArea(
-          child: BlocBuilder<ProductByCategoryCubit, ProductsByCategoryState>(
-              builder: (context, state){
-                if (state is ProductsByCategoryLoadingState && state.products.isEmpty){
-                  return Center(child: CircularProgressIndicator(
-                  ),);
-                }
-                if(state is ProductsByCategoryErrorState  && state.products.isEmpty){
-                  return Center(
-                    child: Text(state.errorMessage),
-                  );
-                }
-                if(state is ProductsByCategoryLoadedState && state.products.isEmpty){
-                  return Center(
-                    child: Text("No products found"),
-                  );
-                }
-                return ListView.builder(itemBuilder: itemBuilder);
-              })),
+      body: SafeArea(child:
+          BlocBuilder<ProductByCategoryCubit, ProductsByCategoryState>(
+              builder: (context, state) {
+        if (state is ProductsByCategoryLoadingState && state.products.isEmpty) {
+          return Skeletonizer(
+            child: Column(
+              children: [
+                const Flexible(
+                    flex: 1, fit: FlexFit.tight, child: ProductGridForDetails())
+              ],
+            ),
+          );
+        }
+        if (state is ProductsByCategoryErrorState && state.products.isEmpty) {
+          return Center(
+            child: Text(state.errorMessage),
+          );
+        }
+        if (state is ProductsByCategoryLoadedState && state.products.isEmpty) {
+          return Center(
+            child: Text("No products found"),
+          );
+        }
+        return Column(
+          children: [
+            const Flexible(
+                flex: 1, fit: FlexFit.tight, child: ProductGridForDetails())
+          ],
+        );
+      })),
     );
   }
 }
