96601f2a10ff3b31099a23d723a411d3505ccb9b
piro-piyush <piyush72717272@gmail.com>
2024-11-10 17:12:34 +0530

    user profile section created

--- Frontend/lib/presentation/widgets/bottom_nav_bar.dart
+++ Frontend/lib/presentation/widgets/bottom_nav_bar.dart
@@ -1,8 +1,12 @@
 import 'package:ecommerce/core/ui.dart';
+import 'package:ecommerce/logic/cubits/user_cubit/user_cubit.dart';
+import 'package:ecommerce/logic/cubits/user_cubit/user_state.dart';
 import 'package:ecommerce/presentation/screens/home/category_screen.dart';
 import 'package:ecommerce/presentation/screens/home/home_screen.dart';
 import 'package:ecommerce/presentation/screens/home/profile_screen.dart';
+import 'package:ecommerce/presentation/screens/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
@@ -16,37 +20,44 @@ class BottomNavBar extends StatefulWidget {
 class _BottomNavBarState extends State<BottomNavBar> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3, // Number of tabs
-      child: Scaffold(
-        bottomNavigationBar: Container(
-          color: AppColors.textLight,
-          height: 70,
-          child: TabBar(
-              tabs: const [
-                Tab(
-                  icon: Icon(Icons.home),
-                  text: "Home",
-                ),
-                Tab(
-                  icon: Icon(Icons.category_outlined),
-                  text: "Category",
-                ),
-                Tab(
-                  icon: Icon(Icons.person),
-                  text: "Profile",
-                ),
-              ],
-              indicatorColor: AppColors.text,
-              labelColor: AppColors.text,
-              unselectedLabelColor: AppColors.white),
-        ),
-        body: const TabBarView(
-          children: [
-            HomeScreen(),
-            CategoryScreen(),
-            ProfileScreen(),
-          ],
+    return BlocListener<UserCubit, UserState>(
+      listener: (context, state) {
+        if (state is UserLoggedOutState) {
+          Navigator.pushReplacementNamed(context, SplashScreen.routeName);
+        }
+      },
+      child: DefaultTabController(
+        length: 3, // Number of tabs
+        child: Scaffold(
+          bottomNavigationBar: Container(
+            color: AppColors.textLight,
+            height: 70,
+            child: TabBar(
+                tabs: const [
+                  Tab(
+                    icon: Icon(Icons.home),
+                    text: "Home",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.category_outlined),
+                    text: "Category",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.person),
+                    text: "Profile",
+                  ),
+                ],
+                indicatorColor: AppColors.text,
+                labelColor: AppColors.text,
+                unselectedLabelColor: AppColors.white),
+          ),
+          body: const TabBarView(
+            children: [
+              HomeScreen(),
+              CategoryScreen(),
+              ProfileScreen(),
+            ],
+          ),
         ),
       ),
     );
